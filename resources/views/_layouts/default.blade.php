f5d8c8b0e0a1550e55af9814480420a990914546
Tyraan T <tomkly000@gmail.com>
2015-07-07 18:07:23 +0800

    Revert "Revert "finish home page & auth""
    This reverts commit 2c1c9fd5da73f5d20a766825b0bd8fb8de40837a.

---
+++ resources/views/_layouts/default.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Aile Test</title>
+<link href="css/add.css" rel="stylesheet" type='text/css'>
+<!--Fonts-->
+<link href='http:..fonts.useso.com/css?family=Roboto:400,300'
+	rel="stylesheet" type='text/css'>
+</head>
+<body>
+
+	<div class="container" style="margin-top: 20px;">
+		@yield('content')
+		<div id="footer" style="text-align: center; border-top:"></div>
+	</div>
+</body>
+</html>
+
