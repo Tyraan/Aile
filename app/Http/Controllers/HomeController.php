d5d0a0c20606c212e4772ed3b26ef48ee6667f1a
Tyraan T <tomkly000@gmail.com>
2015-07-08 12:19:47 +0800

    ddasdasd
    dasdasdas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,11 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
+<<<<<<< HEAD
+		return view('home');
+=======
 		return view('home')->with('Pages',Page::all());
+>>>>>>> parent of 4827d3e... mistake
 	}
 
 }
