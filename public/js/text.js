8b14044c1217c684432762412158c3291e991e2a
Tyraan T <tomkly000@gmail.com>
2015-07-22 18:57:49 +0800

    delete redandant
    delete redandancy

--- public/js/text.js
+++
@@ -1,4 +0,0 @@
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F2da88d73f22b9b5c87b46bd7359497a4' type='text/javascript'%3E%3C/script%3E"));
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fa40ee4d489123b6e1fa1925583338011' type='text/javascript'%3E%3C/script%3E")); 
