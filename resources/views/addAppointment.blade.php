d20a0b760e2b39ea6d806e9f88fbf2efe8dea9b2
Tyraan T <tomkly000@gmail.com>
2015-08-13 17:52:05 +0800

    addAppointment
    addAppointment

--- resources/views/addAppointment.blade.php
+++ resources/views/addAppointment.blade.php
@@ -1,17 +1,32 @@
-<form action="addAppointment_submit" method="POST" accept-charset="utf-8">
+<form action="addAppointment" method="POST" accept-charset="utf-8">
 <table>
 	<caption>免费预约</caption>
-	<thead>
-		<tr>
-			<th>header</th>
-		</tr>
-	</thead>
 	<tbody>
 		<tr>
-			<td>data</td>
+			<td>宝宝姓名</td><td><input type ="text" name = 'babyname'></td>
+		</tr>
+		<tr>
+			<td>宝宝年龄</td><td><input type="time" name="babymoth" ></td>
+		</tr>
+
+		<tr>
+			<td>宝宝性别</td><td><input type="radio" name="babysex" value="男"></td><td><input type="radio" name="babysex" value="女" ></td>
+		</tr>
+		<tr>
+		 	<td>手机号</td><td><input type="tel" name="mobile" ></td>
+		</tr>
+		<tr>
+			<td>所在城市</td><td><input type="text" name="city" ></td>
+		</tr>
+		<tr>
+			<td>中心地址</td><td><input type="text" name="address" ></td>
+		</tr>
+		<tr>
+			<td>预约时间</td><td><input type="datetime" name="pubtime" ></td>
+		</tr>
+		<tr><td>提交</td><td><input type="POST" ></td>
 		</tr>
 	</tbody>
-</table>
-	
+</table>	
 </foPOST>
 
