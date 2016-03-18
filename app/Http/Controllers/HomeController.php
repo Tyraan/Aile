e588c0376b6bb202e51fe51596ac3260cd9d2851
Tyraan T <tomkly000@gmail.com>
2016-03-18 18:48:50 +0800

    add courseList page
    add courseList page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,9 +47,23 @@ class HomeController extends Controller {
 
 	public function postCourse(Request $request)
 	{
-		return view('viewfile/home/course');
-	}
+		$birthday = $request->input('birthday','2016-3-17');
+		$child_age = floor((time()-strtotime($request->input('birthday','2016-3-17')))/31104000);
+		$child_month_age = floor((time()-strtotime($request->input('birthday','2016-3-17')))/2592000);
 
+		$music_course_content= MusicCradleCourse($child_month_age); //
+		$fitbaby_course_content = FitBabyCourse($child_month_age);  //
+		$FitKid_course_content = FitKidCourse($child_month_age); //
+		$Art_course_content = ArtCourse($child_month_age);       //
+		$CookingFun_course_content = CookingFunCourse($child_month_age);//
+		$LetterLand_course_content = LetterLandCourse($child_month_age); //
+		$WaterGym_course_content = WaterGymCourse($child_month_age); //
+
+
+
+
+		return view('viewfile/home/courseList')->with('')
+	}
 	
 
 
