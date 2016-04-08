3f10d33ce27e43978e18f0a92718896466eeb57a
Tyraan T <tomkly000@gmail.com>
2016-04-08 18:50:54 +0800

    add  test ja

--- resources/views/mainPageJsandCss.blade.php
+++ resources/views/mainPageJsandCss.blade.php
@@ -1,6 +1,10 @@
 <link rel="shortcut icon" type="image/x-icon" href="{{  URL::asset('/img/aileguoji.ico') }}" media="screen" />
 
 <script type="text/javascript" charset="utf-8" src="http://lead.soperson.com/20000301/10045318.js"></script>
+<script type="text/javascript" src="/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="/js/jquery.event.drag-1.5.min.js"></script>
+<script type="text/javascript" src="/js/jquery.touchSlider.js"></script>
+
 <script type="text/javascript">
 $(document).ready(function(){
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,11 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <title>ueditor demo</title>
+
 </head>
 
 <body>
-    {{ var_dump($name) }}
+<script type="text/javascript" src="/js/jquery-2.2.2.js"></script>  
+  <script type="text/javascript" charset="utf-8" >
+  $(document).ready(function(){
+    $('#inputimage').click(function(){
+    
+    var requestData ={"action": "listimage", "start": 0, "size": 20};
+    $.get('/UEditor/php/controller.php',requestData,function(data){       
+                
+         var returnedObj =  eval("("+data+")");         
+          if(returnedObj.state == "SUCCESS"){            
+            $('#imageList').empty();
+            var htmlinput = "";
+            //var imageList = eval("("+returnedObj.list+")");
+            $.each(returnedObj.list,function(key,value){              
+               htmlinput+= "<input type='hidden' name='image[]' value="+value+">"
+             });
+            $('#imageList').html(htmlinput);
+          }
+         });
 
+    });
+    });
+
+    </script>
+    {{ var_dump($name) }}
 
     <form action="{{ URL('/test' )}}" method="post">
      {!! csrf_field() !!}
@@ -16,7 +40,16 @@
         <script id="container" name="content" type="text/plain">
             
         </script>
-         <input type="submit" value="提交" > 
+
+        <div id = "imageList" >
+        </div>
+
+
+        <input type="button" id="inputimage" name="imageinput" value="试试获得图片列表">        
+        <input type="submit" value="提交" > 
+
+
+
     </form>
     <!-- 配置文件 -->
     <script type="text/javascript" src="/UEditor/ueditor.config.js"></script>
